1d7366a4ac00439015fcc2b9b7c7b1edcc141860
Rehan Nagoor <rehannags@yahoo.com>
2024-07-24 02:35:36 -0400

    added navbar files

---
+++ lib/data.ts
@@ -0,0 +1,26 @@
+export const links = [
+   {
+     name: "Home",
+     link: "#home",
+   },
+   {
+     name: "About",
+     link: "#about",
+   },
+   {
+     name: "Projects",
+     link: "#projects",
+   },
+   {
+     name: "Skills",
+     link: "#skills",
+   },
+   {
+     name: "Experience",
+     link: "#experience",
+   },
+   {
+     name: "Contact",
+     link: "#contact",
+   },
+ ];

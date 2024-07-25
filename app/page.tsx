31e24c02180d372a09761af39cb596fa13489cf6
Rehan Nagoor <rehannags@yahoo.com>
2024-07-24 23:49:45 -0400

    removed ' for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={links} />
-        <h1>Hey, I'm Rehan Nagoor Mohideen!</h1>
+        <h1>Hey, Im Rehan Nagoor Mohideen!</h1>
 
       </div>
     </main>

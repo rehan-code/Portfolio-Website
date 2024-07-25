f2bd3452a503f61bf588e74342a8a7aa00d40c9c
Rehan Nagoor <rehannags@yahoo.com>
2024-07-25 03:30:51 -0400

    Base intro page complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import { FloatingNav } from "@/components/floatingNavbar";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Intro from "@/components/intro";
 import { links } from "@/lib/data";
 
 export default function Home() {
   return (
     <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+      <div className="">
         <FloatingNav navItems={links} />
-        <h1>Hey, Im Rehan Nagoor Mohideen!</h1>
-
+        <Intro />
       </div>
     </main>
   );

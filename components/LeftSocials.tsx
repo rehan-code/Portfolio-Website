9d996060457b72d32147750e3f6c883a4c0ef6cf
Rehan Nagoor <rehannags@yahoo.com>
2024-08-08 06:36:02 +0400

    added left social visibility values

--- components/LeftSocials.tsx
+++ components/LeftSocials.tsx
@@ -1,5 +1,31 @@
+"use client";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { useState } from "react";
+
 export const LeftSocials = () => {
+  const { scrollYProgress } = useScroll();
+  const [visible, setVisible] = useState(true);
+
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    // Check if current is not undefined and is a number
+    if (typeof current === "number") {
+      let direction = current! - scrollYProgress.getPrevious()!;
+
+      if (scrollYProgress.get() < 0.05) {
+        setVisible(false);
+      } else {
+        if (direction < 0) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+      }
+    }
+  });
+
   return (
+
     <div
       //  orientation="left"
       className="w-7 fixed bottom-6 left-10 right-auto z-10  lg:visible "

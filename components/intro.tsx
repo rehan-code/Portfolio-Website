357de3119b2e939003a01400cf8451bb453e027b
Rehan <rehannags@yahoo.com>
2024-11-11 22:47:44 +0400

    update hover color change

--- components/intro.tsx
+++ components/intro.tsx
@@ -12,8 +12,8 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 // import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
-//   const { ref } = useSectionInView("Home", 0.5);
-//   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  //   const { ref } = useSectionInView("Home", 0.5);
+  //   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <section
@@ -21,19 +21,23 @@ export default function Intro() {
       id="home"
       className="mb-28 max-w-[60rem] pb-6 text-center sm:mb-0 scroll-mt-[100rem] mx-auto"
     >
-
-      <TextGenerateEffect words="Hi! I'm Rehan Nagoor Mohideen" className="text-center text-[80px] text-4xl md:text-7xl lg:text-8xl" duration={0.7}/>
+      <TextGenerateEffect
+        words="Hi! I'm Rehan Nagoor Mohideen"
+        className="text-center text-[80px] text-4xl md:text-7xl lg:text-8xl"
+        duration={0.7}
+      />
 
       <motion.h1
         className="mt-12 px-4 text-lg font-light !leading-[1.5] sm:text-2xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
-         duration: 0.5,
-         delay: 0.5,
+          duration: 0.5,
+          delay: 0.5,
         }}
       >
-        {"A passionate "} <span className="font-semibold">Software Engineer</span>
+        {"A passionate "}{" "}
+        <span className="font-semibold">Software Engineer</span>
       </motion.h1>
 
       <motion.h1
@@ -41,11 +45,12 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
-         duration: 0.5,
-         delay: 0.5,
+          duration: 0.5,
+          delay: 0.5,
         }}
       >
-        <span className="font-semibold">Excited</span> to build {" "} <span className="font-semibold">AI</span>  {" "} projects!
+        <span className="font-semibold">Excited</span> to build{" "}
+        <span className="font-semibold">AI</span> projects!
       </motion.h1>
 
       <motion.div
@@ -79,7 +84,7 @@ export default function Intro() {
         </a>
 
         <a
-          className="bg-white p-4 border-2 dark:border-0 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          className="bg-white p-4 border-2 dark:border-0 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/rehan-nagoor-mohideen-6b3156202/"
           target="_blank"
         >
@@ -87,7 +92,7 @@ export default function Intro() {
         </a>
 
         <a
-          className="bg-white p-4 border-2 dark:border-0 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          className="bg-white p-4 border-2 dark:border-0 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://github.com/rehan-code"
           target="_blank"
         >
